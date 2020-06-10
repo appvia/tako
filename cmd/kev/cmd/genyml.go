b2252f8111d44748a9e6c3896be8b4f7a7f29199
Ezo Saleh <ezo@the-experimenters.com>
2020-06-10 17:59:16 +0100

    Completed basic `kev init` + `kev genyml` CLI commands. (#37)

---
+++ cmd/kev/cmd/genyml.go
@@ -0,0 +1,90 @@
+/**
+ * Copyright 2020 Appvia Ltd <info@appvia.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
+)
+
+var genymlCmd = &cobra.Command{
+	Use:   "genyml",
+	Short: "Spikes generating yml with comments & more.",
+	RunE:  runGenYamlCmd,
+}
+
+type App struct {
+	Name     string   `yaml:"name"`
+	// Services []yaml.Node `yaml:"services"`
+	Services *yaml.Node `yaml:"services,omitempty"`
+}
+
+type Doc struct {
+	App *App `yaml:"app"`
+}
+
+func runGenYamlCmd(cmd *cobra.Command, _ []string) error {
+	out, _ := cmd.Flags().GetString("out")
+	doc := &Doc{
+		App: &App{
+			Name: "hello-world",
+			Services: &yaml.Node{
+				HeadComment: "Start, expected app services",
+				Kind: yaml.SequenceNode,
+				Content: []*yaml.Node{
+					{
+						Kind: yaml.ScalarNode,
+						Value:       "[placeholder]",
+						LineComment: "add service name",
+					},
+					{
+						Kind: yaml.ScalarNode,
+						Value:       "[placeholder]",
+						LineComment: "add service name",
+					},
+				},
+			},
+		},
+	}
+
+	outFile, err := os.Create(out)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	enc := yaml.NewEncoder(outFile)
+	enc.SetIndent(2)
+	return enc.Encode(doc)
+}
+
+func init() {
+	flags := genymlCmd.Flags()
+	flags.SortFlags = false
+
+	flags.StringP(
+		"out",
+		"o",
+		"",
+		"Output destination file",
+	)
+	_ = genymlCmd.MarkFlagRequired("out")
+
+	rootCmd.AddCommand(genymlCmd)
+}
--- go.mod
+++ go.mod
@@ -3,15 +3,21 @@ module github.com/appvia/kube-devx
 go 1.14
 
 require (
+	github.com/compose-spec/compose-go v0.0.0-20200608091248-554a39de0b3a
+	github.com/disiqueira/gotree v1.0.0
+	github.com/fatih/color v1.9.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/goccy/go-yaml v1.7.5
+	github.com/mattn/go-colorable v0.1.6 // indirect
 	github.com/mitchellh/mapstructure v1.3.2 // indirect
 	github.com/pelletier/go-toml v1.8.0 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
-	github.com/spf13/cobra v1.0.0 // indirect
+	github.com/spf13/cobra v1.0.0
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.7.0 // indirect
 	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
 	gopkg.in/ini.v1 v1.57.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c
 )
