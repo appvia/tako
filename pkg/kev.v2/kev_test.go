920576c1fd34a0742f7689184d1cc2e61a04b623
Ezo Saleh <ezo@the-experimenters.com>
2020-07-29 12:35:55 +0100

    MVP (init-labels): re-work init to produce an inline `kev` config without a `.kev` directory. (#171)
    * Completes inline kev config without a .kev directory.
    
    * Includes basic tests to verify kev lib facade.

---
+++ pkg/kev.v2/kev_test.go
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2020 Appvia Ltd <info@appvia.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kev_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/appvia/kube-devx/pkg/kev.v2"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestManifestMarshalsCorrectly(t *testing.T) {
+	files := []string{"testdata/in-cluster-wordpress/docker-compose.yaml"}
+	manifest, err := kev.Init(files, []string{})
+	if err != nil {
+		t.Fatalf("Unexpected error:\n%s", err)
+	}
+
+	var actual bytes.Buffer
+	if _, err := manifest.WriteTo(&actual); err != nil {
+		t.Fatalf("Unexpected error:\n%s", err)
+	}
+
+	expected, err := ioutil.ReadFile("testdata/in-cluster-wordpress/kev.yaml")
+	if err != nil {
+		t.Fatalf("Unexpected error:\n%s", err)
+	}
+
+	diff := cmp.Diff(actual.Bytes(), expected)
+	if diff != "" {
+		t.Fatalf("actual does not match expected\n%s", diff)
+	}
+}
+
+func TestInitProvidesEnvironmentConfig(t *testing.T) {
+	files := []string{"testdata/in-cluster-wordpress/docker-compose.yaml"}
+	manifest, err := kev.Init(files, []string{})
+	if err != nil {
+		t.Fatalf("Unexpected error:\n%s", err)
+	}
+
+	var actual bytes.Buffer
+	env, err := manifest.GetEnvironment("dev")
+	if err != nil {
+		t.Fatalf("Unexpected error:\n%s", err)
+	}
+	if _, err := env.WriteTo(&actual); err != nil {
+		t.Fatalf("Unexpected error:\n%s", err)
+	}
+
+	expected, err := ioutil.ReadFile("testdata/in-cluster-wordpress/docker-compose.kev.dev.yaml")
+	if err != nil {
+		t.Fatalf("Unexpected error:\n%s", err)
+	}
+
+	diff := cmp.Diff(actual.Bytes(), expected)
+	if diff != "" {
+		t.Fatalf("actual does not match expected:\n%s", diff)
+	}
+}
