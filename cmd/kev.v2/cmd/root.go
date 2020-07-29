bb0f70fdebeb72ddf69ac85d9f64284da58ef132
Ezo Saleh <ezo@the-experimenters.com>
2020-07-29 12:35:55 +0100

    MVP (init-labels): re-work init to produce an inline `kev` config without a `.kev` directory. (#171)
    * Completes inline kev config without a .kev directory.
    
    * Includes basic tests to verify kev lib facade.

---
+++ cmd/kev.v2/cmd/root.go
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2020 Appvia Ltd <info@appvia.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+const banner = `
+o
+| /
+OO   o-o o   o
+| \  |-'  \ /
+o  o o-o   o
+
+`
+
+var silentErr = errors.New("silentErr")
+var rootCmd = &cobra.Command{
+	Use:   "kev2",
+	Short: "Reuse and run your Docker Compose applications on Kubernetes",
+	Long: `(kev) transforms your Docker Compose applications
+                  into Cloud Native applications you can run on Kubernetes.`,
+	SilenceErrors: true,
+	SilenceUsage:  true,
+}
+
+func init() {
+	fmt.Print(banner)
+
+	// This is required to help with error handling from RunE , https://github.com/spf13/cobra/issues/914#issuecomment-548411337
+	rootCmd.SetFlagErrorFunc(func(cmd *cobra.Command, err error) error {
+		cmd.Println(err)
+		cmd.Println(cmd.UsageString())
+		return silentErr
+	})
+}
+
+// Execute command
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		if err != silentErr {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		os.Exit(1)
+	}
+}
