ec695b91d9afc4db7b2b9739a2196cc6366edb1d
Ezo Saleh <ezo@the-experimenters.com>
2020-07-29 19:50:02 +0100

    Create label docker-compose.kev.<env>.yaml files per kev environment - completes #169. (#173)

--- pkg/kev.v2/types.go
+++ pkg/kev.v2/types.go
@@ -20,11 +20,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"path"
+	"path/filepath"
+
+	composego "github.com/compose-spec/compose-go/types"
 )
+
+type composeProject struct {
+	version string
+	*composego.Project
+}
+
+type labels struct {
+	Version  string `yaml:"version,omitempty" json:"version,omitempty"`
+	Services composego.Services
+	Volumes  composego.Volumes
+}
 
 type Manifest struct {
 	Sources      []string     `yaml:"compose,omitempty" json:"compose,omitempty"`
 	Environments Environments `yaml:"environments,omitempty" json:"environments,omitempty"`
+	labels       labels
 }
 
 // WriteTo writes out a manifest to a writer.
@@ -49,6 +65,61 @@ func (m *Manifest) GetEnvironment(name string) (Environment, error) {
 	return Environment{}, fmt.Errorf("no such environment: %s", name)
 }
 
+// ExtractLabels extracts the base set of labels from the manifest's docker-compose source files.
+func (m *Manifest) ExtractLabels() (*Manifest, error) {
+	ready, err := newComposeProject(m.Sources)
+	if err != nil {
+		return nil, err
+	}
+	m.labels = extractLabels(ready)
+	return m, nil
+}
+
+// extractLabels same as ExtractLabels but works on a compose project object.
+func extractLabels(c *composeProject) labels {
+	out := labels{
+		Version: c.version,
+	}
+	extractVolumesLabels(c, &out)
+
+	for _, s := range c.Services {
+		target := composego.ServiceConfig{
+			Name:   s.Name,
+			Labels: map[string]string{},
+		}
+		setDefaultLabels(&target)
+		extractServiceTypeLabels(s, &target)
+		extractDeploymentLabels(s, &target)
+		extractHealthcheckLabels(s, &target)
+		out.Services = append(out.Services, target)
+	}
+	return out
+}
+
+// MintEnvironments create new environments based on candidate environments and manifest base labels.
+// If no environments are provided, a default environment will be created.
+func (m *Manifest) MintEnvironments(candidates []string) *Manifest {
+	m.Environments = Environments{}
+	if len(candidates) == 0 {
+		candidates = append(candidates, defaultEnv)
+	}
+	for _, env := range candidates {
+		m.Environments = append(m.Environments, Environment{
+			Name:    env,
+			content: m.labels,
+			File:    path.Join(m.GetWorkingDir(), fmt.Sprintf(configFileTemplate, env)),
+		})
+	}
+	return m
+}
+
+func (m *Manifest) GetWorkingDir() string {
+	if len(m.Sources) < 1 {
+		return ""
+	}
+	return filepath.Dir(m.Sources[0])
+}
+
 type Environments []Environment
 
 // MarshalYAML makes Environments implement yaml.Marshaler.
@@ -71,13 +142,17 @@ func (e Environments) MarshalJSON() ([]byte, error) {
 
 type Environment struct {
 	Name    string `yaml:"-" json:"-"`
-	Content []byte `yaml:"-" json:"-"` // TODO: this will become a compose.VersionedProject
 	File    string `yaml:"-" json:"-"`
+	content labels
 }
 
 // WriteTo writes out an environment to a writer.
 // The Environment struct implements the io.WriterTo interface.
 func (e Environment) WriteTo(w io.Writer) (n int64, err error) {
-	written, err := w.Write(e.Content)
+	data, err := MarshalIndent(e.content, 2)
+	if err != nil {
+		return int64(0), err
+	}
+	written, err := w.Write(data)
 	return int64(written), err
 }
