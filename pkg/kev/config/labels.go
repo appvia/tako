af29509af94188e23bbc97630066914cf3256695
marcinc <marcin.ciszak@appvia.io>
2020-09-29 13:04:48 +0100

    fixup! Adds autsocaling related labels and defaults

--- pkg/kev/config/labels.go
+++ pkg/kev/config/labels.go
@@ -29,11 +29,11 @@ const (
 	// LabelWorkloadAutoscaleMaxReplicas represents maximum number of replicas for given workload. Used in Horizontal Pod Autoscaler.
 	LabelWorkloadAutoscaleMaxReplicas = "kev.workload.autoscale-max-replicas"
 
-	// LabelWorkloadAutoscalingCPUUtilizationThreshold represents a maximum CPU utilization for given workload that instructs horizontal pod autoscaler.
-	LabelWorkloadAutoscalingCPUUtilizationThreshold = "kev.workload.autoscale-cpu-threshold"
+	// LabelWorkloadAutoscaleCPUUtilizationThreshold represents a maximum CPU utilization for given workload that instructs horizontal pod autoscaler.
+	LabelWorkloadAutoscaleCPUUtilizationThreshold = "kev.workload.autoscale-cpu-threshold"
 
-	// LabelWorkloadAutoscalingMemoryUtilizationThreshold represents a maximum Memory utilization for given workload that instructs horizontal pod autoscaler.
-	LabelWorkloadAutoscalingMemoryUtilizationThreshold = "kev.workload.autoscale-mem-threshold"
+	// LabelWorkloadAutoscaleMemoryUtilizationThreshold represents a maximum Memory utilization for given workload that instructs horizontal pod autoscaler.
+	LabelWorkloadAutoscaleMemoryUtilizationThreshold = "kev.workload.autoscale-mem-threshold"
 
 	// LabelWorkloadRollingUpdateMaxSurge max number of nodes updated at once
 	LabelWorkloadRollingUpdateMaxSurge = "kev.workload.rolling-update-max-surge"
