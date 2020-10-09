8172c1c2e7d1eb4a8f42dca131ee73582f2f8a0f
Ezo Saleh <ezo@the-experimenters.com>
2020-10-09 10:57:34 +0100

    Can now check an app is running for wordpress-mysql.

--- e2e/helper.bash
+++ e2e/helper.bash
@@ -1,3 +1,19 @@
+#
+# Copyright 2020 Appvia Ltd <info@appvia.io>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 setup() {
   export TMP="$BATS_TEST_DIRNAME/../tmp"
   export E2E_NS="e2e-$(uuidgen | tr "[:upper:]" "[:lower:]")"
@@ -77,3 +93,18 @@ ensure-service-type(){
   $KUBECTL -n ${namespace} get service -l ${label} --no-headers | grep -i ${type}
 }
 
+check-app-is-running(){
+  local namespace=$1
+  local label=$2
+  local port=$3
+
+  for ((i=0; i<=60; i++)); do
+    if eval "$KUBECTL -n ${namespace} exec $($KUBECTL get pod -n "${namespace}" -l "${label}" -o name) --  curl -sLI localhost:${port}" ; then
+      return 0
+    else
+      sleep 3
+    fi
+  done
+
+  return 1
+}
