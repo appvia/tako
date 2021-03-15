24282d642bd89f6a1dd29e49cb4a36a03844cb82
Ezo Saleh <ezo@the-experimenters.com>
2021-03-15 16:14:57 +0000

    Fix tests after rebase.

--- pkg/kev/init_test.go
+++ pkg/kev/init_test.go
@@ -214,6 +214,7 @@ services:
   db:
     labels:
       kev.workload.liveness-probe-command: '["CMD", "echo", "Define healthcheck command for service db"]'
+      kev.workload.liveness-probe-type: command
       kev.workload.replicas: "1"
 volumes:
   db_data:
