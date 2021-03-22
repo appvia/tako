8e85788d7114f177ed3ed3635f5f27de55574017
Filipe Azevedo <filipe@azevedo.io>
2021-03-22 12:08:38 +0000

    move schema error handling

--- pkg/kev/services.go
+++ pkg/kev/services.go
@@ -152,37 +152,6 @@ func (sc ServiceConfig) validate() error {
 	return errors.New(result.Errors()[0].Description())
 }
 
-func excludeTypes(ts ...string) func(gojsonschema.ResultError) bool {
-	return func(re gojsonschema.ResultError) bool {
-		for _, t := range ts {
-			if t == re.Type() {
-				return false
-			}
-		}
-		return true
-	}
-}
-
-func withType(t string) func(gojsonschema.ResultError) bool {
-	return func(re gojsonschema.ResultError) bool {
-		return t == re.Type()
-	}
-}
-
-func findError(result *gojsonschema.Result, predicate func(re gojsonschema.ResultError) bool) gojsonschema.ResultError {
-	if result.Valid() {
-		return nil
-	}
-
-	for _, e := range result.Errors() {
-		if predicate(e) {
-			return e
-		}
-	}
-
-	return nil
-}
-
 // GetLabels gets a service's labels
 func (sc ServiceConfig) GetLabels() map[string]string {
 	return sc.Labels
